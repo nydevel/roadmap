ebe3d1278f2a08f7a367853cd29fcbdc22c127ca
Nikolay Yasinskiy <talk@shfu.ru>
2022-12-22 13:06:15 +0500

    Add axum

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html("hello world")
 }
