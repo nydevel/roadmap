c5d50770c4d59ceb989267e219d37f31c9b735c1
Nikolay Yasinskiy <talk@shfu.ru>
2022-12-24 21:33:27 +0500

    Add dotenv parsing

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,13 +1,20 @@
-use std::net::SocketAddr;
-
 use axum::{response::Html, routing::get, Router};
+use dotenv::dotenv;
+use std::{env, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    dotenv().ok();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
+    let app = Router::new().route("/", get(root));
+    let addr = env::var("SERVER_ADDRESS")
+        .unwrap_or_else(|e| panic!("Failed to get env with name 'SERVER_ADDRESS': {:?}", e));
+
+    let parsed_address: SocketAddr = addr
+        .parse()
+        .unwrap_or_else(|e| panic!("Failed to parse socket address: {:?}", e));
+
+    axum::Server::bind(&parsed_address)
         .serve(app.into_make_service())
         .await
         .unwrap();
