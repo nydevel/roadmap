c33e317c7f7a2c8e9ec4d0ef673f1901b43a389e
Nikolay Yasinskiy <talk@shfu.ru>
2022-12-29 13:54:12 +0500

    Add sqlx support, need to refactor it

--- back/src/main.rs
+++ back/src/main.rs
@@ -1,4 +1,5 @@
 use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use std::env;
 
 mod adapters;
@@ -8,6 +9,21 @@ mod routes;
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    // TODO: remove this block to separate adapter
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:changeme@localhost/test")
+        .await
+        .unwrap();
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+    println!("some value: {:?}", row);
 
     let addr = env::var("SERVER_ADDRESS")
         .unwrap_or_else(|e| panic!("Failed to get env with name 'SERVER_ADDRESS': {:?}", e));
